727f17c2c67e96b0ad6f7953dc5a9e06c0c140e1
Anurag <anubhav.bhardwaj_cs18@gla.ac.in>
2020-03-28 01:55:08 +0530

    Problem 1 Completed

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -7,5 +7,25 @@
 package problem1.main;
 // executable class
 
+import problem1.mybst.MyBinarySearchTree;
+import problem4.myqueue.MyQueue;
+
 public class MyMain {
+    public static void main(String[] args) {
+        MyQueue preQueue = new MyQueue();
+        MyQueue postQueue = new MyQueue();
+        MyBinarySearchTree myBinarySearchTree = new MyBinarySearchTree(preQueue, postQueue);
+        myBinarySearchTree.insert(15);
+        myBinarySearchTree.insert(10);
+        myBinarySearchTree.insert(50);
+        myBinarySearchTree.insert(55);
+        myBinarySearchTree.insert(60);
+        myBinarySearchTree.postOrder(myBinarySearchTree.getRoot());
+        myBinarySearchTree.preOrder(myBinarySearchTree.getRoot());
+        System.out.println("PreOrder:");
+        preQueue.show();
+        System.out.println("PostOrder:");
+        postQueue.show();
+        System.out.println();
+    }
 }
